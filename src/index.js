22c3a9476ee0aa1d5e732891e6d53caadebf221b
Geoffrey Gazet <geoffrey.gazet@consertotech.pro>
2018-07-30 11:12:20 +0200

    Init commit of todolist

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// ========================================
+
+class TodoList extends React.Component {
+    render() {
+        <div>To do list</div>
+    }
+}
+
+ReactDOM.render(
+    <TodoList />,
+    document.getElementById('root')
+);
+  
